e7c6a214d1e3d27a87c2d9e02c7f713551f2274c
Eric Lima <eric.george.lima@gmail.com>
2020-03-04 08:44:44 +0000

    first commit

---
+++ request.py
@@ -0,0 +1,19 @@
+# importing the requests library 
+import requests 
+
+# api-endpoint 
+URL = "https://dre.pt/web/guest/home/-/dre/calendar/normal/I?day=2020-03-04&date=2020-03-01"
+
+# location given here 
+location = "dre.pt"
+
+# defining a params dict for the parameters to be sent to the API 
+PARAMS = {'address':location} 
+
+# sending get request and saving the response as response object 
+r = requests.get(url = URL, params = PARAMS) 
+
+# Documento em formato PDF
+
+print(r.content)
+
