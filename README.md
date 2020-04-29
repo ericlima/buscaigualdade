fc71d34fa475b471d29098cc98da5e4dc6cfa516
eric.george.lima@gmail.com <eric.george.lima@gmail.com>
2020-04-29 05:41:46 +0100

    final

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # buscaigualdade
 
-run config.sh to install python3 packages
+#ter o python3 instalado
+sudo apt install python3
+
+sudo apt-get install build-essential libpoppler-cpp-dev pkg-config python-dev
+
+#na primeira vez instalar pipenv
+sudo apt install pipenv
+
+pipenv install
+
+pip install pdftotext
--- request.py
+++ request.py
@@ -5,12 +5,20 @@ import urllib.request as urllib2
 
 import datetime
 
+import os
+
+import sys
+
 def download_file(url):
-    elements = url.split("/")
-    pdf_file = urllib2.urlopen(url)
-    with open(elements[-1]+'.pdf','wb') as output:
-        output.write(pdf_file.read())
-    return
+    try:
+        elements = url.split("/")
+        pdf_file = urllib2.urlopen(url)
+        with open(DOWNLOADS + elements[-1]+'.pdf','wb') as output:
+            output.write(pdf_file.read())
+        return elements[-1]
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        return ""
 
 inicio = datetime.date.today() - datetime.timedelta(days=30)
 fim = datetime.date.today()
@@ -26,6 +34,8 @@ date_list = [base - datetime.timedelta(days=x) for x in range(numdays)]
 # api-endpoint 
 URL = "https://dre.pt/web/guest/home/-/dre/calendar/normal/II?day=" #2020-03-06&date=2020-03-01"
 
+DOWNLOADS = 'Downloads/'
+
 # location given here 
 location = "dre.pt"
 
@@ -34,7 +44,7 @@ PARAMS = {'address':location}
 
 for d in date_list:
     url2 = URL + d.isoformat()
-    print(url2)
+    #print(url2)
 
     # sending get request and saving the response as response object 
     r = requests.get(url = url2, params = PARAMS) 
@@ -49,8 +59,13 @@ for d in date_list:
 
     link = "https://dre.pt"+referencia[:pos2]
 
-    download_file(link)
+    arq = download_file(link)
 
-    
+    if (len(arq)>1):
+        print(arq)
+        text = os.popen("pdftotext {}".format(DOWNLOADS + arq + '.pdf')).read()
+        with open(DOWNLOADS + arq + '.txt') as f:
+            if 'Porto Seguro' in f.read():
+                print("porto seguro = true")
 
-
+    #print(text)
